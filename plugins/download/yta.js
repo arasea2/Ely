edc67d9b4cceed7619324142ac54c6712354be6f
arasea2 <144518692+arasea2@users.noreply.github.com>
2024-01-08 11:47:19 +0000

    update ytplay

--- plugins/download/yta.js
+++ plugins/download/yta.js
@@ -9,13 +9,13 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	if (isUrl(text)) {
 		url = text
 		try {
-			let anu = await yts( { videoId: await ytdl.getURLVideoID(url) } )
+			let anu = await yts({ videoId: await ytdl.getURLVideoID(url) })
 			let txt = `📌 *${anu.title}*\n\n`
-			+ `🪶 *Author :* ${anu.author.name}\n`
-			+ `⏲️ *Published :* ${anu.ago}\n`
-			+ `⌚ *Duration :* ${anu.duration.timestamp}\n`
-			+ `👁️ *Views :* ${anu.views}\n`
-			+ `🌀 *Url :* ${url}`
+				+ `🪶 *Author :* ${anu.author.name}\n`
+				+ `⏲️ *Published :* ${anu.ago}\n`
+				+ `⌚ *Duration :* ${anu.duration.timestamp}\n`
+				+ `👁️ *Views :* ${anu.views}\n`
+				+ `🌀 *Url :* ${url}`
 			await conn.sendMsg(m.chat, { image: { url: anu.thumbnail }, caption: txt }, { quoted: m })
 		} catch (e) {
 			console.log(e)
@@ -23,11 +23,11 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 				let anu = await youtubeSearch(url)
 				anu = anu.video[0]
 				let txt = `📌 *${anu.title}*\n\n`
-				+ `🪶 *Author :* ${anu.authorName}\n`
-				+ `⏲️ *Published :* ${anu.publishedTime}\n`
-				+ `⌚ *Duration :* ${anu.durationH}\n`
-				+ `👁️ *Views :* ${anu.viewH}\n`
-				+ `🌀 *Url :* ${anu.url}`
+					+ `🪶 *Author :* ${anu.authorName}\n`
+					+ `⏲️ *Published :* ${anu.publishedTime}\n`
+					+ `⌚ *Duration :* ${anu.durationH}\n`
+					+ `👁️ *Views :* ${anu.viewH}\n`
+					+ `🌀 *Url :* ${anu.url}`
 				await conn.sendMsg(m.chat, { image: { url: anu.thumbnail.split("?")[0] }, caption: txt }, { quoted: m })
 			} catch (e) {
 				console.log(e)
@@ -40,11 +40,11 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 			anu = anu.all[0]
 			url = anu.url
 			let txt = `📌 *${anu.title}*\n\n`
-			+ `🪶 *Author :* ${anu.author.name}\n`
-			+ `⏲️ *Published :* ${anu.ago}\n`
-			+ `${(anu.duration && anu.duration.timestamp) ? `⌚ *Duration :* ${anu.duration.timestamp}\n` : ''}`
-			+ `👁️ *Views :* ${anu.views}\n`
-			+ `🌀 *Url :* ${url}`
+				+ `🪶 *Author :* ${anu.author.name}\n`
+				+ `⏲️ *Published :* ${anu.ago}\n`
+				+ `${(anu.duration && anu.duration.timestamp) ? `⌚ *Duration :* ${anu.duration.timestamp}\n` : ''}`
+				+ `👁️ *Views :* ${anu.views}\n`
+				+ `🌀 *Url :* ${url}`
 			await conn.sendMsg(m.chat, { image: { url: anu.thumbnail }, caption: txt }, { quoted: m })
 		} catch (e) {
 			console.log(e)
@@ -53,11 +53,11 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 				anu = anu.video[0]
 				url = anu.url
 				let txt = `📌 *${anu.title}*\n\n`
-				+ `🪶 *Author :* ${anu.authorName}\n`
-				+ `⏲️ *Published :* ${anu.publishedTime}\n`
-				+ `⌚ *Duration :* ${anu.durationH}\n`
-				+ `👁️ *Views :* ${anu.viewH}\n`
-				+ `🌀 *Url :* ${url}`
+					+ `🪶 *Author :* ${anu.authorName}\n`
+					+ `⏲️ *Published :* ${anu.publishedTime}\n`
+					+ `⌚ *Duration :* ${anu.durationH}\n`
+					+ `👁️ *Views :* ${anu.viewH}\n`
+					+ `🌀 *Url :* ${url}`
 				await conn.sendMsg(m.chat, { image: { url: anu.thumbnail.split("?")[0] }, caption: txt }, { quoted: m })
 			} catch (e) {
 				console.log(e)
@@ -66,24 +66,31 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 		}
 	}
 	if (!url) return
+	m.reply('Proses download mp3 memerlukan beberapa menit')
 	try {
-		let res = await youtubedl(url)
-		let data = res.audio[Object.keys(res.audio)[0]]
-		let site = await data.download()
-		if (data.fileSize > 400000) return m.reply(`Filesize: ${data.fileSizeH}\nTidak dapat mengirim, maksimal file 400 MB`)
-		await conn.sendMsg(m.chat, { audio: { url: site }, mimetype: 'audio/mpeg' }, { quoted : m })
+		let res = await ytdl.getURLVideoID(url)
+		let anu = await ytdl.getInfo(res)
+		anu = anu.formats.filter(v => v.mimeType.includes('audio/mp4'))[0]
+		let size = parseInt(anu.contentLength)
+		if (size > 400000000) return m.reply(`Filesize: ${niceBytes(size)}\nTidak dapat mengirim, maksimal file 400 MB`)
+		await conn.sendMsg(m.chat, { audio: { url: anu.url }, mimetype: 'audio/mpeg' }, { quoted: m })
 	} catch (e) {
 		console.log(e)
 		try {
-			let res = await ytdl.getURLVideoID(url)
-			let anu = await ytdl.getInfo(res)
-			anu = anu.formats.filter(v => v.mimeType.includes('audio/mp4'))[0]
-			let size = parseInt(anu.contentLength)
-			if (size > 400000000) return m.reply(`Filesize: ${niceBytes(size)}\nTidak dapat mengirim, maksimal file 400 MB`)
-			await conn.sendMsg(m.chat, { audio: { url: anu.url }, mimetype: 'audio/mpeg' }, { quoted : m })
+			let res = await youtubedl(url)
+			let data = res.audio[Object.keys(res.audio)[0]]
+			let site = await data.download()
+			if (data.fileSize > 400000) return m.reply(`Filesize: ${data.fileSizeH}\nTidak dapat mengirim, maksimal file 400 MB`)
+			await conn.sendMsg(m.chat, { audio: { url: site }, mimetype: 'audio/mpeg' }, { quoted: m })
 		} catch (e) {
 			console.log(e)
-			m.reply(e)
+			try {
+				let anu = await (await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${api.lol}&url=${url}`)).json()
+				await conn.sendMsg(m.chat, { audio: { url: anu.result.link }, mimetype: 'audio/mpeg' }, { quoted: m })
+			} catch (e) {
+				console.log(e)
+				m.reply('Server Down')
+			}
 		}
 	}
 }
