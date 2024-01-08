2ceaa620470ffae6c4bdce9f27a8af94e5303658
arasea2 <144518692+arasea2@users.noreply.github.com>
2024-01-08 15:14:21 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
 export default {
   apps: [{
     name: 'Ely',
-    script: 'index.mjs',
+    script: 'index.js',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    exp_backoff_restart_delay: 100,
     env: {
       NODE_ENV: 'production'
     },
