f976211d1389c1c6704528ec8bc902d8e7dfe342
arasea2 <144518692+arasea2@users.noreply.github.com>
2024-01-09 00:53:09 +0000

    update main

--- main.js
+++ main.js
@@ -159,46 +159,34 @@ console.log((os.totalmem - os.freemem) / (1023 / 1024))
 //}, 100)
 
 const appName = 'index.js';
-const memoryThreshold = 100; // Batas memori dalam megabyte
-
-pm2.connect(() => {
-  pm2.launchBus((err, bus) => {
-    bus.on('process:event', (packet) => {
-      if (packet.process.name === appName && packet.event === 'exit') {
-        console.error(`${appName} exited unexpectedly. Restarting...`);
-        restartApp();
-      }
-    });
-  });
-
-  pm2.start({
-    script: './index.js',
-    name: appName,
-    autorestart: false, // Menonaktifkan restart otomatis PM2
-  }, (startErr) => {
-    if (startErr) {
-      console.error(startErr);
-      pm2.disconnect();
-    }
-  });
-});
+const memoryThreshold = 200; // Batas memori dalam megabyte
 
 function restartApp() {
-  pm2.restart(appName, (restartErr) => {
-    if (restartErr) {
-      console.error(restartErr);
-    }
-  });
+	pm2.restart(appName, (restartErr) => {
+		if (restartErr) {
+			console.error(restartErr);
+		}
+	});
 }
+pm2.connect(() => {
+	pm2.launchBus((err, bus) => {
+		bus.on('process:event', (packet) => {
+			if (packet.process.name === appName && packet.event === 'exit') {
+				console.error(`${appName} exited unexpectedly. Restarting...`);
+				restartApp();
+			}
+		});
+	});
+})
 
 setInterval(() => {
-  pm2.describe(appName, (descErr, description) => {
-    if (!descErr && description[0]) {
-      const memoryUsage = description[0].monit.memory / (1024 * 1024); // Memori dalam megabyte
-      if (memoryUsage < memoryThreshold) {
-        console.error(`Memory usage below ${memoryThreshold} MB. Restarting...`);
-        restartApp();
-      }
-    }
-  });
+	pm2.describe(appName, (descErr, description) => {
+		if (!descErr && description[0]) {
+			const memoryUsage = description[0].monit.memory / (1024 * 1024); // Memori dalam megabyte
+			if (memoryUsage < memoryThreshold) {
+				console.error(`Memory usage below ${memoryThreshold} MB. Restarting...`);
+				restartApp();
+			}
+		}
+	});
 }, 60000); // Memeriksa setiap 30 detik
