d48f741bb1697f8328c37a3f4aa3aff9accec708
arasea2 <144518692+arasea2@users.noreply.github.com>
2024-01-09 05:55:16 +0000

    update yts

--- plugins/download/ytv.js
+++ plugins/download/ytv.js
@@ -2,10 +2,10 @@ import ytdl from 'ytdl-core'
 import { niceBytes } from '../../lib/func.js'
 import { youtubedl } from '@bochilteam/scraper-sosmed'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-	if(!args[0]) throw 'Masukkan Link'
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+	if(!text) throw 'Masukkan Link'
 	try {
-		let anu = await (await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${api.lol}&url=${args[0]}`)).json()
+		let anu = await (await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${api.lol}&url=${text[0]}}`)).json()
 		await conn.sendFile(m.chat, anu.result.link, `${anu.result.title}.mp4`, txt, m)
 	} catch (e) {
 		m.reply('invalid url / internal server error.')
